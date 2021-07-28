e604b11984546b44a5c3e30925d74421130b9263
hknowlton <heather.knowlton@carvana.com>
2021-07-28 14:39:59 -0400

    working form

--- app.js
+++ app.js
@@ -16,29 +16,144 @@ app.message('hello', async ({ message, say }) => {
     blocks: [
       {
         type: 'section',
+        fields: [
+          {
+            type: 'plain_text',
+            text: 'Hello! What team would you like to make a saying for?',
+            emoji: true
+          }
+        ]
+      },
+      {
+        type: 'divider'
+      },
+      {
+        type: 'section',
         text: {
           type: 'mrkdwn',
-          text: `Hey there <@${message.user}>!`
+          text: 'Team name'
         },
         accessory: {
-          type: 'button',
-          text: {
+          type: 'static_select',
+          placeholder: {
             type: 'plain_text',
-            text: 'Click Me'
+            text: 'Select a user',
+            emoji: true
           },
-          action_id: 'button_click'
+          options: [
+            {
+              text: {
+                type: 'plain_text',
+                emoji: true,
+                text: 'DMP'
+              },
+              value: 'value-0'
+            },
+            {
+              text: {
+                type: 'plain_text',
+                emoji: true,
+                text: 'Vortal'
+              },
+              value: 'value-1'
+            },
+            {
+              text: {
+                type: 'plain_text',
+                emoji: true,
+                text: 'Annotations'
+              },
+              value: 'value-2'
+            }
+          ],
+          action_id: 'team-select-action'
         }
       }
-    ],
-    text: `Hey there <@${message.user}>!`
+    ]
   });
 });
 
-app.action('button_click', async ({ body, ack, say }) => {
+app.action('team-select-action', async ({ body, ack, say }) => {
   // Acknowledge the action
   await ack();
-  await say(`<@${body.user.id}> clicked the button`);
+
+  await say(
+    `Nice team, I have always wanted to be on ${body.actions[0].selected_option.text.text}!`
+  );
+
+  await say({
+    blocks: [
+      {
+        dispatch_action: true,
+        type: 'input',
+        element: {
+          type: 'plain_text_input',
+          action_id: 'new_saying-action'
+        },
+        label: {
+          type: 'plain_text',
+          text: `Enter a new saying for ${body.actions[0].selected_option.text.text}`,
+          emoji: true
+        }
+      }
+    ]
+  });
 });
+
+app.action('new_saying-action', async ({ body, ack, say }) => {
+  await ack();
+  await say(`Nice joke, I think ${body.actions[0].value} will really slap`);
+  await say({
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'plain_text',
+          text: 'Is this an error or success message?',
+          emoji: true
+        }
+      },
+      {
+        type: 'actions',
+        elements: [
+          {
+            type: 'radio_buttons',
+            options: [
+              {
+                text: {
+                  type: 'plain_text',
+                  text: 'error',
+                  emoji: true
+                },
+                value: 'error'
+              },
+              {
+                text: {
+                  type: 'plain_text',
+                  text: 'success',
+                  emoji: true
+                },
+                value: 'success'
+              }
+            ],
+            action_id: 'error_or_success'
+          }
+        ]
+      }
+    ]
+  });
+});
+
+app.action('error_or_success', async ({ body, ack, say }) => {
+  await ack();
+  await say('Great! Sending this off to Wavy HQ now.');
+});
+
+// app.action('new_saying-action', async ({ body, ack, say }) => {
+//   // Acknowledge the action
+//   await ack();
+//   await say(`Nice joke, I think ${body.actions[0].value} will really slap`);
+// });
 
 (async () => {
   // Start your app
