5b76c5d288f3ffe8d43e53f2c422607a4bd89cbb
hknowlton <heather.knowlton@carvana.com>
2021-07-28 11:51:53 -0400

    hello world for the app using bolt

---
+++ app.js
@@ -0,0 +1,23 @@
+const { App } = require('@slack/bolt');
+require('dotenv').config();
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true, // enable the following to use socket mode
+  appToken: process.env.SLACK_APP_TOKEN
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
