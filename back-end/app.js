c82142670e4280ca62b8a625ff971558386b2f56
Aaron <Sosa>
2021-07-29 14:45:19 -0400

    - making changes

--- back-end/app.js
+++ back-end/app.js
@@ -1,22 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const { express: { port, parser } } = require('./config');
 const { Team, Saying } = require('./config/sequelize')
 
 const configure = async (app) => {
   app.use(bodyParser.urlencoded(parser.urlencoded));
   app.use(bodyParser.json(parser.json));
+
+  app.use(cors('*'))
+
   app.get('/teams', async (req, res, next) => {
     const payload = await Team.findAll()
     const response = payload.map(d => d.toJSON())
     res.send(response)
-  })
-  app.get('/teams/:TeamId/sayings', async (req, res, next) => {
-    const { TeamId } = req.params;
-    const payload = await Saying.findAll({ where: { TeamId } })
+  });
+
+  app.get('/teams/:TeamName/sayings', async (req, res, next) => {
+    const { TeamName } = req.params;
+    const payload = await Saying.findAll({ iniclude: { model: Team, where: { Name: TeamName } } });
     const response = payload.map(d => d.toJSON())
-    res.send(response)
-  })
+    const renderResponse = response.reduce((renderedObj, res) => {
+      switch (res.Type) {
+        case 1:
+          renderedObj.Success.push(res.Name)
+          break;
+        case 2:
+          renderedObj.Failure.push(res.Name)
+          break;
+        default:
+          break;
+      }
+      return renderedObj;
+    }, { Success: [], Failure: [] })
+    res.send(renderResponse)
+  });
+
   app.post('/teams/:TeamId/sayings', async (req, res, next) => {
     const { TeamId } = req.params;
     const { Message, Type } = req.body;
@@ -24,6 +43,7 @@ const configure = async (app) => {
     const response = payload.toJSON()
     res.send(response)
   })
+
   return app;
 };
 
